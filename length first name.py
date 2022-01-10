d807912a68ac134994eb80e1b671c8c90dead75a
Mohamed_Abdulsalam <moha2016it@gmail.com>
2022-01-10 15:27:06 +0300

    length first name

---
+++ length first name.py
@@ -0,0 +1,12 @@
+# #hi! My name is: Mohamed Abdulsalam..
+#practice cource..
+# #challeange (3.2)
+"""
+Ask the user to enter their first name and then display the length of their name.
+ upload program file to GitHub and past the link.
+
+"""
+
+
+firstName = input("Enter First name :")
+print("the length of first name is : (" + str(len(firstName)) + ")")
